fa9e37ad1fafbd8e83cbe7fd34e9a6cfdeb814ec
munir247e <munirhossain821451@gmail.comm>
2024-07-05 22:16:21 +0600

    assignment

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,7 @@
+
+<?php
+$celsius = 32; // Store the temperature in Celsius
+$fahrenheit = ($celsius * 9/5) + 32; // Convert to Fahrenheit
+
+printf("The temperature is %.2f degrees Fahrenheit.", $fahrenheit);
+?>
